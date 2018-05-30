60a72b148e257eacc42963042a78c4b453c32812
PC140518 <PC140518@DESKTOP-1JSSCJS>
2018-05-30 13:23:34 +0200

    done

--- MSCH/MainWindow.xaml.cs
+++ MSCH/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
 
 namespace MSCH
 {
@@ -20,9 +21,56 @@ namespace MSCH
     /// </summary>
     public partial class MainWindow : Window
     {
+            
+        [DllImport("User32.dll")]
+        static extern Boolean SystemParametersInfo(
+            UInt32 uiAction,
+            UInt32 uiParam,
+            UInt32 pvParam,
+            UInt32 fWinIni);
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //CHANGE const MOUSESPEED 
+        private void Button_Fast(object sender, RoutedEventArgs e)
+        {
+            const UInt32 SPI_SETMOUSESPEED = 0x0071;
+            const UInt32 MOUSESPEED = 10;
+
+            SystemParametersInfo(
+                SPI_SETMOUSESPEED,
+                0,
+                MOUSESPEED,
+                0);
+
+        }
+
+        private void Button_Slow(object sender, RoutedEventArgs e)
+        {
+            const UInt32 SPI_SETMOUSESPEED = 0x0071;
+            const UInt32 MOUSESPEED = 1;
+
+            SystemParametersInfo(
+                SPI_SETMOUSESPEED,
+                0,
+                MOUSESPEED,
+                0);
+        }
+
+        private void Button_Normal(object sender, RoutedEventArgs e)
+        {
+            const UInt32 SPI_SETMOUSESPEED = 0x0071;
+            const UInt32 MOUSESPEED = 5;
+
+            SystemParametersInfo(
+                SPI_SETMOUSESPEED,
+                0,
+                MOUSESPEED,
+                0);
         }
     }
 }
